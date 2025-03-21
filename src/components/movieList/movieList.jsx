c0b1b3b75b94b61d0643a14ad59116055262e8bb
Dhiraj Londhe <dhirajdipaklondhe4@gmail.com>
2025-03-21 15:12:34 +0530

    Did some changes

--- src/components/movieList/movieList.jsx
+++ src/components/movieList/movieList.jsx
@@ -3,6 +3,7 @@ import { useStore } from "../../store/store.js";
 import { MovieCard } from "./movieCard";
 import { useDebounce } from "../../helpers/useDebounce.js";
 import { fetchMovieByName } from "../../helpers/fetchMovieByName.js";
+import { NotFound } from "../notFound.jsx";
 
 export function MovieList() {
 
@@ -11,13 +12,13 @@ export function MovieList() {
     const [ list, setList ] = useState([]);
 
     useEffect(useDebounce(async function() {
-        const data = await fetchMovieByName(movieName);  
+        const data = await fetchMovieByName(movieName);
         setList(data);
-    }),[movieName, movieID, isName]);
+    },500),[movieName, movieID, isName]);
     
     return (//poster, title, type, year, imdbID
         <div className="flex gap-5 flex-wrap">
-            {
+            {/* {
                 list && list.map((e) => <MovieCard 
                                             poster={e.Poster}
                                             title={e.Title}
@@ -26,6 +27,21 @@ export function MovieList() {
                                             year={e.Year}
                                             imdbID={e.imdbID}
                                         />)
+            } */}
+
+            {
+                (list == 'error') ? <div>Somthing Went Wrong</div> :
+
+                ((list) ? list.map((e) => <MovieCard 
+                                        poster={e.Poster}
+                                        title={e.Title}
+                                        Type={e.Type}
+                                        key={e.imdbID}
+                                        year={e.Year}
+                                        imdbID={e.imdbID}
+                                        />) : 
+                                        <NotFound/>
+                )
             }
         </div>
 
